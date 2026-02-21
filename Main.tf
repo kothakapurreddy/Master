1262eb6f262530040242a8fd6ff8e44ef6416ab2
kothakapurreddy <kothakapurreddy@gmail.com>
2026-02-21 17:47:59 +0530

    Main.tf

---
+++ Main.tf
@@ -0,0 +1,60 @@
+# main.tf
+
+provider "google" {
+  project = "YOUR_PROJECT_ID"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+# 1️⃣ VPC
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = false
+}
+
+# 2️⃣ Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  network       = google_compute_network.vpc_network.id
+  region        = "us-central1"
+}
+
+# 3️⃣ Firewall - Allow SSH, HTTP, ICMP
+resource "google_compute_firewall" "fw" {
+  name    = "my-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22","80"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# 4️⃣ VM Instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 20
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.subnet.name
+    access_config {}
+  }
+
+  tags = ["http-server","ssh"]
+}
